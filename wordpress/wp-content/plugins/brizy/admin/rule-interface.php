46dc20d98099faf8850c06876dcfdbdc206d056d
JAVA <jose.avalos@utec.edu.pe>
2019-12-25 22:29:46 -0500

    actualizar web wp2

--- wordpress/wp-content/plugins/brizy/admin/rule-interface.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-interface Brizy_Admin_RuleInterface {
-	public function isMatching( $applyFor, $entityType, $entityValues );
-
-	public function getRuleWeight();
-}
--- wordpress/wp-content/plugins/brizy/public/views/templates/brizy-header-footer-template.php
+++
@@ -1,7 +0,0 @@
-<?php defined( 'ABSPATH' ) or die();
-
-get_header();
-
-do_action( 'brizy_template_content' );
-
-get_footer();
